90855b894e6fbad06a190446b708af600b916f15
DannyPhongcoderso1 <dannyphong2407@gmail.com>
2026-03-16 09:13:47 +0700

    fixed DB connection

--- CloudContactManager/Program.cs
+++ CloudContactManager/Program.cs
@@ -64,6 +64,11 @@ else
 
 var app = builder.Build();
 
+    using (var scope = app.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        db.Database.EnsureCreated();
+    }
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
